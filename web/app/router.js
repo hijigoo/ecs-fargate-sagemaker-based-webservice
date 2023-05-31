a3cf44cd8d41c52d50f4811f44d9403fa1e5911d
KICHUL KIM <hi.jigoo@gmail.com>
2023-05-31 22:26:47 +0900

    feat: Add was health check to web

--- web/app/router.js
+++ web/app/router.js
@@ -17,9 +17,20 @@ router.get("/version", function (req, res) {
 
 // Redirect
 const request = require('request');
-BASE_URL = "http://internal-chatbot-backend-lb-540268964.ap-northeast-2.elb.amazonaws.com"
+BASE_URL = "http://app-was-alb-1501545287.us-west-2.elb.amazonaws.com"
 
 CLASSIFIY_IMAGE = "/classify/image"
+HEALTH = "/health"
+router.get("/was/health", function (req, res) {
+  let url = BASE_URL + HEALTH
+  request.get({
+    url: url,
+  },function(err,response,body){
+    console.log(body)
+    return res.json({"was-health":JSON.parse(body)})
+  })
+  res.json({"version": "0.0.1"});
+});
 
 router.post('/classify/image', multer().single('file'), function(req, res) {
   console.log("/classify/sentence")
