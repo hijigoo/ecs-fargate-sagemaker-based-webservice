dfb4b51da1eb37067ee2214abcd9e93a111e4b82
KICHUL KIM <hi.jigoo@gmail.com>
2023-06-01 13:08:05 +0900

    feat: Update was code

--- was/app/main.py
+++ was/app/main.py
@@ -11,7 +11,7 @@ from dex_util import *
 app = FastAPI()
 
 origins = [
-    "[SET YOUR BACKEND URL]"
+    "http://app-web-alb-440880009.us-west-2.elb.amazonaws.com"
 ]
 
 app.add_middleware(
--- web/app/router.js
+++ web/app/router.js
@@ -17,7 +17,7 @@ router.get("/version", function (req, res) {
 
 // Redirect
 const request = require('request');
-BASE_URL = "http://app-was-alb-1501545287.us-west-2.elb.amazonaws.com"
+BASE_URL = "http://internal-app-was-alb-1863869934.us-west-2.elb.amazonaws.com"
 
 CLASSIFIY_IMAGE = "/classify/image"
 HEALTH = "/health"
