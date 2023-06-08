7375259a7430ee4724b4abb87d0a9d7bb1baf4f3
ksdyb.park <ksdyb@amazon.com>
2023-06-09 07:31:36 +0900

    update

--- cdk-ai-wep-application/README.md
+++ cdk-ai-wep-application/README.md
@@ -5,3 +5,31 @@
 ```java
 cdk init app --language typescript
 ```
+
+## Docker
+
+```java
+docker login --username AWS --password-stdin https://[account-id].dkr.ecr.ap-northeast-2.amazonaws.com
+```
+
+## Reference
+
+[class Vpc (construct)](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.Vpc.html)
+
+[Creating an AWS Fargate service using the AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/ecs_example.html)
+
+[Introduction to the Cloud Development Kit (CDK)](https://catalog.us-east-1.prod.workshops.aws/workshops/5962a836-b214-4fbf-9462-fedba7edcc9b/en-US)
+
+[VPC: enum SubnetType](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.SubnetType.html)
+
+[CDK Construct library for higher-level ECS Constructs](https://docs.aws.amazon.com/cdk/api/v1/docs/aws-ecs-patterns-readme.html)
+
+[class ApplicationLoadBalancedFargateService (construct)](https://docs.aws.amazon.com/cdk/api/v1/docs/@aws-cdk_aws-ecs-patterns.ApplicationLoadBalancedFargateService.html)
+
+[Deploying applications to ECS Fargate with AWS CDK](https://www.gravitywell.co.uk/insights/deploying-applications-to-ecs-fargate-with-aws-cdk/)
+
+[How to setup AutoScale for ECS service using CDK](https://medium.com/@alexander.sirenko/setup-autoscale-for-ecs-service-using-cdk-39f03cc3f046)
+
+[class SecurityGroup (construct)](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.SecurityGroup.html)
+
+[Security Group Examples in AWS CDK - Complete Guide](https://bobbyhadz.com/blog/aws-cdk-security-group-example)
--- cdk-ai-wep-application/lib/cdk-ai-wep-application-stack.ts
+++ cdk-ai-wep-application/lib/cdk-ai-wep-application-stack.ts
@@ -1,16 +1,145 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as path from "path";
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+// import * as ecrdeploy from 'cdk-ecr-deployment';
+// import * as aws_elasticloadbalancingv2 from 'aws-cdk-lib/ aws_elasticloadbalancingv2';
+import * as logs from "aws-cdk-lib/aws-logs"
+
 
 export class CdkAiWepApplicationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC, Subnet
+    const vpc = new ec2.Vpc(this, 'my-app-vpc', {
+      maxAzs: 2, // Default is all AZs in region
+      natGateways: 2,
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      vpcName : 'app-vpc',
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC, // PUBLIC, PRIVATE_ISOLATED, PRIVATE_WITH_EGRESS
+          cidrMask: 20,
+          name: 'public'
+        },
+        {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 20,
+          name: 'private'
+        },
+      ]
+    }); 
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAiWepApplicationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    // Security Group - app-web-alb-sg
+    const sg_WebAlb = new ec2.SecurityGroup(this, "AppWebAlbSg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: 'security group',
+      securityGroupName: "app-web-alb-sg",
+    }); 
+    sg_WebAlb.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );  
+
+    // Security Group - app-web-sg
+    const sg_Web = new ec2.SecurityGroup(this, "AppWebSg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: 'security group',
+      securityGroupName: "app-web-sg",
+    }); 
+    sg_Web.addIngressRule(
+      // ec2.Peer.anyIpv4(),
+      ec2.Peer.securityGroupId(sg_WebAlb.securityGroupId),
+      ec2.Port.tcp(8000),
+      'allow HTTP traffic from anywhere',
+    );  
+
+    const webImage = ecs.ContainerImage.fromAsset('../web');
+    const wasImage = ecs.ContainerImage.fromAsset('../was'); 
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'ServiceTask', {
+      family: 'ServiceTask'
+    });
+
+    const webContainerDefinition = taskDefinition.addContainer('Web', {
+      image: webImage,
+      portMappings: [{ containerPort: 80 }]
+    });
+    const wasContainerDefinition = taskDefinition.addContainer('Was', {
+      image: wasImage,
+      portMappings: [{ containerPort: 8000 }]
+    });
+
+    // ecs cluster
+    const cluster = new ecs.Cluster(this, "AppEcsCluster", {
+      vpc: vpc
+    }); 
+
+    // Create an application load-balanced Fargate service and make it public
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+    //  circuitBreaker: {
+    //    rollback: true,
+    //  }, 
+      cpu: 512, // Default is 256
+      desiredCount: 1, // Default is 1
+      taskImageOptions: { 
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+      //  image: webImage,
+      //  containerPort: 80,
+      //  logDriver: ecs.LogDrivers.awsLogs({
+      //    streamPrefix: id,
+      //    logRetention: logs.RetentionDays.ONE_YEAR,
+      //  }), 
+      },
+    //  taskDefinition: taskDefinition,
+      securityGroups: [sg_WebAlb, sg_Web],
+      memoryLimitMiB: 2048, // Default is 512
+      // loadBalancerName
+      // publicLoadBalancer: true, // Default is true
+    }); 
+
+    fargateService.targetGroup.configureHealthCheck({
+      path: "/health",
+    });
+
+    const scalableTaskCount = fargateService.service.autoScaleTaskCount({
+      minCapacity: 2,
+      maxCapacity: 4,
+    });
+
+    scalableTaskCount.scaleOnCpuUtilization('CpuUtilizationScaling', {
+      targetUtilizationPercent: 70,      
+      scaleInCooldown: cdk.Duration.seconds(300),
+      scaleOutCooldown: cdk.Duration.seconds(300),
+    });  
+  } 
 }
+
+
+
+  /*  const image = new DockerImageAsset(this, 'CDKDockerImageforWeb', {
+      directory: path.join(__dirname, '../../web'),
+    }); */
+
+  /*  const tg = new elbv2.ApplicationTargetGroup(this, 'TG', {
+      targetType: elbv2.TargetType.IP,
+      port: 50051,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      protocolVersion: elbv2.ApplicationProtocolVersion.GRPC,
+      healthCheck: {
+        enabled: true,
+        healthyGrpcCodes: '0-99',
+      },
+      vpc,
+    }); */    
+
+// const image = ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample";    
